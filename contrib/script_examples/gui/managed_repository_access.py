8f1beaca19387cf38d93b503a7a899cdd96c0a43
tobias-schlauch <>
2011-03-26 14:13:25 +0100

    The demosntrates all relevant actions. The following have been added: - Reading / writing properties - Creation of links - Copy / move Operations - Reading / wrting data
    The operations are also performed using item names with non-ASCII characters.

--- contrib/script_examples/gui/managed_repository_access.py
+++ contrib/script_examples/gui/managed_repository_access.py
@@ -1,3 +1,4 @@
+# @description: Performs tests using data store "Data Store" and data type "Directory".
 # $Filename$ 
 # $Authors$
 # Last Changed: $Date$ $Committer$ $Revision-Id$
@@ -42,14 +43,23 @@ The standard hello world example.
 
 import datetime
 import logging
+import StringIO
+import unicodedata
         
 from datafinder.gui.user import script_api
-from datafinder.script_api.error import ItemSupportError
 from datafinder.script_api.repository import setWorkingRepository
-from datafinder.script_api.item.item_support import createLeaf
+from datafinder.script_api.item import item_support
+from datafinder.script_api.properties import property_support
 
 
 __version__ = "$Revision-Id:$" 
+
+
+_DATA_STORE_NAME = "Data Store" # Change this to use another data store
+# Change this to use another data type
+# The data type should allow recursive definitions
+_DATA_TYPE_NAME = "Directory" 
+_SMALL_AE = unicodedata.lookup("LATIN SMALL LETTER A WITH DIAERESIS")
 
                 
 _log = logging.getLogger("script")
@@ -63,17 +73,49 @@ if not mr is None:
     _log.info("Creating test file test.txt..")
     script_api.lock(["/"])
     def _createLeaf():
-        properties = dict()
-        properties["____dataformat____"] = "TEXT"
-        properties["____datastorename____"] = "Data Store"
-        properties["____contentmodificationdatetime____"] = datetime.datetime.now()
-        properties["____contentcreationdatetime____"] = datetime.datetime.now()
-        properties["____content.size____"] = 0
-        
-        try:
-            createLeaf("/test.txt", properties)
-        except ItemSupportError, error:
-            _log.error(error.message)
+        for _ in range(5):
+            collectionPaths = [u"/test"  + _SMALL_AE, u"/test" + _SMALL_AE + "/sub test"]
+    
+            # Create test collection structure and a test file
+            for path in collectionPaths: 
+                item_support.createCollection(path, {"____datatype____": _DATA_TYPE_NAME})
+            
+            leafPath = collectionPaths[0] + u"/test" + _SMALL_AE + ".txt"
+            properties = dict()
+            properties["____dataformat____"] = "TEXT"
+            properties["____datastorename____"] = "Data Store"
+            properties["____contentmodificationdatetime____"] = datetime.datetime.now()
+            properties["____contentcreationdatetime____"] = datetime.datetime.now()
+            properties["____content.size____"] = 0
+            item_support.createLeaf(leafPath, properties)
+            
+            # Read and write some data
+            item_support.storeData(leafPath, StringIO.StringIO("test..."))
+            _log.info(item_support.retrieveData(leafPath).read())
+            
+            # Tests link creation and access
+            linkPath = collectionPaths[0] + "/leaf_link.txt"
+            item_support.createLink(linkPath, leafPath)
+            
+            # Read and write some properties
+            properties = {
+                "string": "string", "date": datetime.datetime.now(),
+                "boolean": True, "number": 10.7, 
+                "list": ["string", 10, True, datetime.datetime.now()]}
+            property_support.storeProperties(leafPath, properties)
+            _log.info(property_support.retrieveProperties(leafPath))
+            
+            # Copy and move it
+            copiedLeafPath = collectionPaths[1] + "/test.txt"
+            item_support.copy(leafPath, copiedLeafPath)
+            _log.info(item_support.retrieveData(copiedLeafPath).read())
+            movedLeafPath = collectionPaths[1] + "/test_2.txt"
+            item_support.move(leafPath, movedLeafPath)
+            _log.info(item_support.retrieveData(movedLeafPath).read())
+            
+            # Delete created collections/files
+            item_support.delete(collectionPaths[0])
+    
     def _cb():
         script_api.unlock(["/"])
         script_api.selectItem("/test.txt")
